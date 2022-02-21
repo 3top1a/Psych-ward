0e707daefaba32b21906b808adbde420d76f80dd
3top1a <3top1a.official@gmail.com>
2022-02-21 22:33:16 +0100

    Restructure

--- Character.gd
+++ Character.gd
@@ -1,26 +1,30 @@
 extends KinematicBody
 
 export var speed = 1.5
+export var mouse_sensitivity = 1
 
 var velocity = Vector3(0,0,0)
 
 func _physics_process(delta):
 	var direction = Vector3.ZERO
 	if Input.is_action_pressed("move_right"):
-		direction.x += 1
+		direction += global_transform.basis.x
 	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-	
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		$Pivot.look_at(translation + direction, Vector3.UP)
+		direction += -global_transform.basis.x
+	if Input.is_action_pressed("move_down"):
+		direction += global_transform.basis.z
+	if Input.is_action_pressed("move_up"):
+		direction += -global_transform.basis.z
+	direction.normalized()
 	
 	velocity.x = direction.x * speed
 	velocity.z = direction.z * speed
 	velocity.y = 0
 	
-	velocity = move_and_slide_with_snap(velocity, Vector3(0.01,0.01,0.01), Vector3(0, 1, 0))
+	velocity = move_and_slide_with_snap(velocity, Vector3(0.01,0.01,0.01), Vector3(0, 1, 0), true)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		$Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
+		$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2, 1.2)
