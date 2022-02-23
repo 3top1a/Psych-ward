767a7d816997f73c6aa2ea07854ea51f09cdf6f7
3top1a <3top1a.official@gmail.com>
2022-02-23 20:55:30 +0100

    First enemy not work

--- SkullHead.gd
+++ SkullHead.gd
@@ -1,5 +1,14 @@
 extends RigidBody
 
+export var speed = 5
+export var rot_speed = 5
 
 func _physics_process(delta):
-	pass
+	var target_position = get_tree().get_root().get_node("Game/Player/Character").global_transform
+	
+	global_translate(
+		global_transform.origin.direction_to(get_tree().get_root().get_node("Game/Player/Character").global_transform.origin)
+		* speed * delta)
+	
+	if global_transform.origin.y < 0.5:
+		queue_free()
