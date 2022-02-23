87a04fc2afa91ee13742db908059a8faa5f026e6
3top1a <3top1a.official@gmail.com>
2022-02-23 10:38:41 +0100

    Colors, textures

--- Bullet.gd
+++ Bullet.gd
@@ -2,6 +2,9 @@ extends RigidBody
 
 func _ready():
 	apply_impulse(transform.basis.z, -transform.basis.z * 20)
+	randomize()
+	$One.modulate = Color.darkgoldenrod.from_hsv(rand_range(0, 360), rand_range(0.5, 0.9), rand_range(0.4, 0.9))
+	$Two.modulate = Color.darkgoldenrod.from_hsv(rand_range(0, 360), rand_range(015, 0.5), rand_range(0.1, 0.5))
 
 func _physics_process(delta):
 	if global_transform.origin.y < 0.5:
