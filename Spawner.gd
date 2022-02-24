9dbe9f2bf9af7e11c10ea938baf0019093e0b73b
3top1a <3top1a.official@gmail.com>
2022-02-24 10:22:29 +0100

    Spawner

--- Spawner.gd
+++ Spawner.gd
@@ -1,2 +1,24 @@
 extends Spatial
 
+
+export (Curve) var skulls
+export var skull_pre = preload("res://Prefbs/SkullHead.tscn")
+export (float) var skull_time = 10.0
+export (float) var skull_dist = 80
+export (float) var skull_amount = 10
+var skull_timer = 0.0
+
+var time = 0.0
+
+func _process(delta):
+	time += delta
+	
+	skull_timer += delta
+	if skull_timer > skull_time:
+		skull_timer = 0
+		
+		for x in range(skull_amount):
+			var i = skull_pre.instance()
+			i.global_transform.origin = $"../Player/Character".global_transform.origin + (Vector3.FORWARD * skull_dist).rotated(Vector3.UP, rand_range(0, 360))
+			
+			add_child(i)
